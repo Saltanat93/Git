70d626bbe4302d7af2ec1d7ebf23917a886866cc
Saltanat93 <kasabolotova93@gmail.com>
2017-06-06 18:36:28 -0400

    Testing

---
+++ src/git/git.java
@@ -0,0 +1,15 @@
+package git;
+
+public class git {
+	public static void main(String[] args) {
+		
+	}
+	public static void main(Integer[] args) {
+		
+	}
+	public static void main(Character[] args) {
+		
+	}
+	String name="Saltanat";
+
+}
